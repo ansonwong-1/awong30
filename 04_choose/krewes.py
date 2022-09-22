9df3d58df6e38d3456d6fa8f9e4631291b3a7222
Anson Wong <awong30@stuy.edu>
2022-09-22 09:24:11 -0400

    changed summary to our current method

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -5,9 +5,21 @@ Softdev P2
 '''
 DISCO:
 QCC:
-OPS SUMMARY: We decided to make a function that randomly chose a 
-key and then randomly chose an element from the value list.
+OPS SUMMARY: We decided to make a list of the keys and then randomly choose from that list
+because they aren't numerically sequential. After that, we got a random index for both 
+the list of keys and the list of values. 
 '''
 
-krewes = {2:['1', '2', '3'], 7:['4', '5', '6'], 8:['7', '8', '9']} 
+import math
+import random
+
+krewes = {2:['1', '2', '3'], 7:['4', '5', '6'], 8:['7', '8', '9']}
+
+listofkeys= []
+for i in range(len(krewes)):
+    listofkeys.append(krewes.index(i))
+keyIndice = random.randint(0,2)
+valueIndice = random.randint(0,2)
+
+print(krewes.get(listofkeys[keyIndice])[valueIndice])
 print(krewes[2])
