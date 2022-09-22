9dda7e7514c6ca7da83c0c41d6df11a5a2141de9
Anson Wong <awong30@stuy.edu>
2022-09-22 09:15:01 -0400

    i hate faiza

---
+++ 04_choose/krewes.py
@@ -0,0 +1,13 @@
+'''
+SS AFK | Anson Wong, Faiza Huda, Kevin Xiao, Truthful Tom, Faizem, FamousMrTable 
+Softdev P2
+'''
+'''
+DISCO:
+QCC:
+OPS SUMMARY: We decided to make a function that randomly chose a 
+key and then randomly chose an element from the value list.
+'''
+
+krewes = {2:['1', '2', '3'], 7:['4', '5', '6'], 8:['7', '8', '9']} 
+print(krewes[2])
