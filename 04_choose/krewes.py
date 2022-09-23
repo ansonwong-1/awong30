0e89d4356514c2a31e96dc35feb4fc9f4db34d1d
Anson Wong <awong30@stuy.edu>
2022-09-23 09:23:06 -0400

    had it print out the period the devo was in

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -23,9 +23,6 @@ the list of keys and the list of values. With those, we got a random devo in a r
 
 import random
 
-# original placeholder krewes
-#krewes = {2:['1', '2', '3'], 7:['4', '5', '6'], 8:['7', '8', '9']}
-
 krewes = {
            2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"], 
            7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "KAREN"],
@@ -33,10 +30,35 @@ krewes = {
          }
 
 
-listofkeys= list(krewes.keys()) # for when there are keys
+listofkeys= list(krewes.keys()) # for when there are keys - didn't work with original krewes dictionary copied from carepkg
 keyIndice = random.randint(0, len(listofkeys)-1)
 
 valueIndice = random.randint(0, len(krewes[listofkeys[keyIndice]])-1)
+
+print(krewes[listofkeys[keyIndice]][valueIndice] + " Of Period " + str(keyIndice))
+
+allDevos = []
+for i in krewes:
+    for j in krewes[i]:
+        allDevos.append(j)
+    
+#print(allDevos)
+
+indice = random.randint(0,len(allDevos)-1)
+period = 2
+for i in krewes:
+    #print(i)
+    if (indice < len(krewes[i])):
+        #print(len(krewes[i]))
+        period = i
+
+print(allDevos[indice] + " Of Period " + str(period)) #not sure if this works -Anson
+
+
+# jack and jill, went up the hill, to fetch a pail of water. So they say, the subsequent fall was inevitable, they never stood a chance they were written that way/ 
+
+# original placeholder krewes
+#krewes = {2:['1', '2', '3'], 7:['4', '5', '6'], 8:['7', '8', '9']}
 
 #print(listofkeys)
 
@@ -47,22 +69,9 @@ valueIndice = random.randint(0, len(krewes[listofkeys[keyIndice]])-1)
 # key values
 #print(krewes[listofkeys[keyIndice]])
 # the random value
-print(krewes[listofkeys[keyIndice]][valueIndice])
 
 # checking if it seems random by running it multiple times
 #for i in range(100):
 #    keyIndice = random.randint(0,2)
 #    valueIndice = random.randint(0,2)
 #    print(krewes[listofkeys[keyIndice]][valueIndice])
-
-allDevos = []
-for i in krewes:
-    for j in krewes[i]:
-        allDevos.append(j)
-print(allDevos)
-
-indice = random.randint(0,len(allDevos)-1)
-print(allDevos[indice])
-
-
-# jack and jill, went up the hill, to fetch a pail of water. So they say, the subsequent fall was inevitable, they never stood a chance they were written that way/ 
