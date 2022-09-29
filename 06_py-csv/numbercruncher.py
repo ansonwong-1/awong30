8318115502f088f16e8a5009481942a0140b66c6
Anson <awong30@stuy.edu>
2022-09-29 19:20:49 -0400

    added random part

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -2,15 +2,26 @@
 Bobem | Ivan Yeung, Anson Wong
 SoftDev
 K06 -- Dictionary & Random with Weights
-Time spent:
+Time spent: 1
 DISCO:
 - The function str.rsplit is like str.split but from the right
 - You can add an optional value to split and rsplit to say how many times you want to split
+- You can add weights to the function random.choices (but not random.choice)
+- The weights must be a list of either integers or floats.
+- Each element in the list given in the first parameter of random.choices
+corresponds to the weight of the same index.
+(ex. random.choices([a, b, c], weights = [10, 20, 30]) a has a weight of 10, b has a weight of 20,...)
 QCC:
-- The .csv file is formatted differently in atom (editor) and on github
+- The .csv file is formatted differently in atom (editor) and on github. Why?
+- How can we test for if the weights work correctly?
+- Is it better to do lines = file.read().split('\n') than lines = file.readlines()?
+- In what cases would we need to read the \n?
 OPS SUMMARY:
-We read the file and made each line (w 1 occupation & %) a element in a list
-
+We read the file and made each line (w 1 occupation & %) an element in a list
+Then we split each line once at the first comma from the right
+We then used those values to make a dictionary with the format {Occupation: Percentage}
+We used random.choices to get a random key (occupation) and the key values as the weights
+(excluding the Occupation: Percentage and Total: 99.8)
 """
 
 import random as rng
@@ -19,14 +30,8 @@ f = open('occupations.csv')
 lines = f.read()
 f.close()
 
-#testing out find
-#test_string = '"Education, training", 6.1'
-#print(test_string.find('"', 1))
-#print(test_string[1:test_string.find('"', 1)])
-#print(test_string[test_string.find('"', 1) + 3:])
+
 lines = lines.split('\n')[:-1] #gets rid of the new lines
-labor_force_list = []
-count = 0
 occ_dict = {}
 
 for line in lines:
@@ -37,7 +42,6 @@ for line in lines:
         occ_dict[occ[0]] = occ[1]
 #print(occ_dict)
 
-occ_list = list(occ_dict.keys())
-print(occ_list)
-print(occ_list[1:-1])
-occ = rng.choices(occ_list[1:-1]) #need to add weights
+key_list = list(occ_dict.keys())
+occ = rng.choices(key_list[1:-1], weights = list(occ_dict.values())[1:-1])[0] #need to add weights
+print(occ)
