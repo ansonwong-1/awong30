3dc5304694eafb86d24b86cb3ddd0186e2e37278
Anson Wong <61435344+ansonwong-1@users.noreply.github.com>
2022-10-04 07:51:36 -0400

    Create hw104.py

---
+++ hw104.py
@@ -0,0 +1,18 @@
+'''
+Anson Wong
+Softdev
+HW due T 10/4 --- K5& K6 Notable Finds
+2022-10-04
+Time spent: 0.5
+'''
+'''
+#K05
+- Some teams used file = open("krewes.txt", "r") (Rando, Green Donkeys)
+What does the second parameter mean? (My team didn't use it and had the same results)
+#K06
+- How to use the join function (Green Donkeys)
+('<what goes in between each element>').join(<list of elements that you want joined>) #returns a string
+- The csv module can be used to read csv files (Erasers)
+Team used csv.reader which returned a list of lists in [Occupation, Percentage] format
+
+'''
