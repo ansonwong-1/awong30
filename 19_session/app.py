a2f571d45b3d54d593b091a86ce101c643e5bdab
Anson <awong30@stuy.edu>
2022-11-06 19:50:17 -0500

    added diag and exceptions

--- 19_session/app.py
+++ 19_session/app.py
@@ -4,31 +4,69 @@
 #2022-11-03
 #time spent: 1
 from flask import Flask,session,request, redirect, url_for,render_template
+import os
 
 # Set the secret key to some random bytes. Keep this really secret!
 app = Flask(__name__)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = os.urandom(32)
 
-
+USER = "user"
+PASS = "pass"
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('login_attempt'))
-    return render_template('login.html')
-@app.route("/login_attempt")
-def login_attempt():
-    if 'username' in session:
-        return f'Logged in as {session["username"]}'
-    return 'You are not logged in'
-
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    print("***DIAG: request.method ***")
+    print(request.method)
+    if request.method == "GET":
+        if 'username' in session:
+            try:
+                return render_template('response.html', response="You are logged in as " + session['username'])
+            except IOError:
+                print("EXCEPTION: No response.html file")
+        try:
+            return render_template("login.html")
+        except IOError:
+            print("EXCEPTION: No login.html file")
+    print("***DIAG: request.form['username'] ***")
+    print(request.form['username'])
+    try:
+        username = request.form['username']
+    except:
+        print("EXCEPTION: No username in form")
+    print("***DIAG: request.form['password'] ***")
+    print(request.form['password'])
+    try:
+        password = request.form['password']
+    except:
+        print("EXCEPTION: No password in form")
+    if username != USER:
+        try:
+            return render_template('login.html', error='Bad username')
+        except IOError:
+            print("EXCEPTION: No login.html file")
+    if password != PASS:
+        try:
+            return render_template('login.html', error='Bad password')
+        except IOError:
+            print("EXCEPTION: No login.html file")
+    session['username'] = username
     return redirect(url_for('login'))
 
-if __name__ == "__main__":
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    if 'username' in session:
+        session.pop('username', None)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
     app.debug = True
     app.run()
