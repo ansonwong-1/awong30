42cf382ca01feb65c5120f69b552e4b169b1e730
Anson Wong <61435344+ansonwong-1@users.noreply.github.com>
2023-04-20 12:51:54 -0400

    Update dommy_consolery.js

--- 29_jsdom/dommy_consolery.js
+++ 29_jsdom/dommy_consolery.js
@@ -1,13 +1,32 @@
-// Team Insert :: Anson Wong, Talia Hsia
+/*
+   your PPTASK:
+   
+   Test drive each bit of code in this file,
+    and insert comments galore, indicating anything
+     you discover,
+    	have questions about,
+    		or otherwise deem notable.
+    		
+    		Write with your future self or teammates in mind.
+    		
+    		If you find yourself falling out of flow mode, consult 
+    		other teams
+    		MDN
+
+   A few comments have been pre-filled for you...
+   
+   (delete this block comment once you are done)
+*/
+
+// Team :: Brian Chen, Anson Wong
 // SoftDev pd2
-// K28 -- Getting more comfortable with the dev console and the DOM
-// 2023-04-19
+// K29
+// 2023-04-20
 // --------------------------------------------------
 
 
 //send diagnostic output to console
 //(Ctrl-Shift-K in Firefox to reveal console)
-//used ctrl-shift-j in edge
 console.log("AYO");
 
 var i = "hello";
@@ -19,6 +38,7 @@ var f = function(x) {
   var j=30;
   return j+x;
 };
+
 
 
 //instantiate an object
@@ -65,32 +85,62 @@ var stripe = function() {
   }
 };
 
-//insert your implementations here for...
-// FIB
-// FAC
-// GCD
-function fact(n){
-  if (n < 2){
-      return 1;
-  }return (n * (fact(n - 1)));
-}
 
-function fib(n){
+
+//insert your implementations here for...
+
+function fact(n){
+  if (n<= 0) 
+      return "Error";
+  else if (n == 1)
+      return 1;
+  else {
+      var output = 1;
+      for (var i = 1; i <= n; i++) {
+          output *=i;
+      }
+      return output;
+  }
+};
+
+var fib = function(n){
   if(n < 2){
       return n;
   }return (fib(n - 1) + fib(n - 2));
-}
+};
 
-const gcd = (n1, n2) =>{
-  if (n1 % n2 == 0){
+function gcd(n1,n2){
+  if (n1%n2==0){
     return n2;
-  }
-  if (n2 % n1 == 0){
+  } if (n2%n1==0){
     return n1;
-  }
+  } 
   return gcd(n2, n1 % n2);
-}
-console.log(gcd(18,24));
+};
+
+//console.log(fib(5));
+
+var dasbut = document.getElementById("fib"); 
+dasbut.addEventListener('click', ()=>{
+  console.log(fib(7));
+  var output = fib(7);
+	addItem(output);
+});
+
+var dasbut = document.getElementById("fact"); 
+dasbut.addEventListener('click', ()=>{
+	console.log(fact(5));
+  var output = fact(5);
+	addItem(output);
+});
+
+var dasbut = document.getElementById("gcd"); 
+dasbut.addEventListener('click', ()=>{
+  var output = gcd(17220,1238);
+	addItem(output);
+});
+
+
 // In addition to the style shown above,
 //  you are encouraged to test drive the "arrow function syntax" as shown below.
 //  Note anything notable.
@@ -98,5 +148,4 @@ const myFxn = (param1, param2) => {
   // body
   return retVal;
 };
-
 
